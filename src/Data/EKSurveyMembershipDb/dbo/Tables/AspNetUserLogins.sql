03da0270b18cab9bb3d52a75ed656b178dfe07a3
SLASHER\KnightSword <agemoets@gmail.com>
2018-07-21 02:51:07 -0500

    Added core and services projects.

---
+++ src/Data/EKSurveyMembershipDb/dbo/Tables/AspNetUserLogins.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[AspNetUserLogins] (
+    [LoginProvider] [nvarchar](128) NOT NULL,
+    [ProviderKey] [nvarchar](128) NOT NULL,
+    [UserId] [nvarchar](128) NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey], [UserId])
+)
+GO
+ALTER TABLE [dbo].[AspNetUserLogins] ADD CONSTRAINT [FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+GO
+CREATE INDEX [IX_UserId] ON [dbo].[AspNetUserLogins]([UserId])
