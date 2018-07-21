d85e98f948017f5b2ea8886da8860ffc55ed6bed
SLASHER\KnightSword <agemoets@gmail.com>
2018-07-21 02:51:07 -0500

    Added core and services projects.

---
+++ src/Data/EKSurveyMembershipDb/dbo/Tables/AspNetRoles.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[AspNetRoles] (
+    [Id] [nvarchar](128) NOT NULL,
+    [Name] [nvarchar](256) NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetRoles] PRIMARY KEY ([Id])
+)
+GO
+CREATE UNIQUE INDEX [RoleNameIndex] ON [dbo].[AspNetRoles]([Name])
