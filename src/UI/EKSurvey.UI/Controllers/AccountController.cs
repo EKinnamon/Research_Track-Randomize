5fb9cdc76c3a62ff65edabd7a9db0a838c68824b
SLASHER\KnightSword <agemoets@gmail.com>
2018-07-22 13:48:58 -0500

    Minor code cleanup and database project change.

--- src/UI/EKSurvey.UI/Controllers/AccountController.cs
+++ src/UI/EKSurvey.UI/Controllers/AccountController.cs
@@ -2,8 +2,10 @@
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+
 using EKSurvey.Core.Models.Identity;
 using EKSurvey.Core.Models.ViewModels.Account;
+
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
@@ -28,26 +30,14 @@ namespace EKSurvey.UI.Controllers
 
         public ApplicationSignInManager SignInManager
         {
-            get
-            {
-                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
-            }
-            private set 
-            { 
-                _signInManager = value; 
-            }
+            get => _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            private set => _signInManager = value;
         }
 
         public ApplicationUserManager UserManager
         {
-            get
-            {
-                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            }
-            private set
-            {
-                _userManager = value;
-            }
+            get => _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            private set => _userManager = value;
         }
 
         //
